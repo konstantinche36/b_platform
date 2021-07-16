2ef518244489bf525b9ed1eccdd3879af26ed898
konstantinche36 <konstantinche36@gmail.com>
2021-07-16 15:35:33 +0300

    Added folder for mat class.

---
+++ b_mat/b_mat_worker.py
@@ -0,0 +1,5 @@
+class BMat:
+    def __init__(self, name: str, width: int, height: int, fill: bool = False, fill_color=None):
+        self.name = name
+        self.width = width
+        self.height = height
